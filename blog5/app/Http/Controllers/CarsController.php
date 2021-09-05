3cca98fef8ab21013a567a7c6da6beabf10acff0
Hamza Saleem <hs5418988@gmail.com>
2021-09-05 15:10:08 -0500

    Cars Crud

---
+++ blog5/app/Http/Controllers/CarsController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Car;
+
+class CarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $cars = Car::all();
+      return view('cars.index', compact('cars'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+      return view('cars.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $carData = new Car();
+      $carData->make = $request->CarName;
+      $carData->model = $request->CarModel;
+      $carData->year_of_make = $request->CarYearOfMake;
+      $carData->no_plate_allot = $request->CarNoPlateAllot;
+      $carData->Vin = $request->CarVin;
+      $carData->save();
+      return redirect('/cars');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      $carData = Car::findOrFail($id);
+      return view('cars.show', compact('carData'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      $carData = Car::findOrFail($id);
+      return view('cars.edit', compact('carData'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $carData = Car::findOrFail($id);
+      $carData->make = $request->CarName;
+      $carData->model = $request->CarModel;
+      $carData->year_of_make = $request->CarYearOfMake;
+      $carData->vin = $request->CarVin;
+      $carData->no_plate_allot = $request->CarNoPlateAllot;
+      $carData->save();
+      return redirect('/cars');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $carData = Car::findOrFail($id);
+      $carData->delete();
+      return redirect('/cars');
+    }
+}
---
+++ blog5/resources/views/cars/create.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Cars Create</h1>
+      <form class="" action="/cars/store" method="post">
+        @csrf
+        <div class="form-group">
+          <label for="CarName">Car Name</label>
+          <input class="form-control" id="CarName" type="text" name="CarName" value="">
+        </div>
+        <div class="form-group">
+          <label for="CarModel">Model</label>
+          <input class="form-control" id="CarModel" type="text" name="CarModel" value="">
+        </div>
+        <div class="form-group">
+          <label for="CarYearOfMake">Year Of Make</label>
+          <input class="form-control" id="CarYearOfMake" type="text" name="CarYearOfMake" value="">
+        </div>
+        <div class="form-group">
+          <label for="CarNoPlateAllot">NoPlateAllot</label>
+          <input class="form-control" id="CarNoPlateAllot" type="text" name="CarNoPlateAllot" value="">
+        </div>
+        <div class="form-group">
+          <label for="CarVin">Vin</label>
+          <input class="form-control" id="CarVin" type="text" name="CarVin" value="">
+        </div>
+        <button class="btn btn-info" type="submit" name="button">Submit</button>
+      </form>
+    </div>
+  </body>
+</html>
