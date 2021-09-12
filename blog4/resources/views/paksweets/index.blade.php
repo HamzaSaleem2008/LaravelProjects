4fa23b0b8715a7ac7dc2115b5ddccd567ef126da
Hamza Saleem <hs5418988@gmail.com>
2021-09-11 20:49:45 -0500

    PakSweets Carousel added

--- blog4/resources/views/paksweets/index.blade.php
+++ blog4/resources/views/paksweets/index.blade.php
@@ -57,6 +57,27 @@
         </div>
       </div>
     </nav>
-    <!-- carousel startes below -->
+
+    <div id="mainCarousel" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img height=""class="d-block w-100" src="{{ asset('images/pic1.jpg') }}" alt="picture1">
+        </div>
+        <div class="carousel-item">
+          <img height="" class="d-block w-100" src="{{ asset('images/pic2.jpg') }}" alt="picture2">
+        </div>
+        <div class="carousel-item">
+          <img height="" class="d-block w-100" src="{{ asset('images/pic3.jpg') }}" alt="picture3">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#mainCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#mainCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
   </body>
 </html>
